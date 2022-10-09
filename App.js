f5360b9934e49b9c2e77984a0c85211a278a8ced
Federico <fedeaacevedo@gmail.com>
2022-10-09 03:42:25 -0300

    title styles

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, StyleSheet} from "react-native";
 
 
 const App = () => {
   
   return (
  
-    <SafeAreaView>
-    <Text>
-      Mi nombre es Federico
-    </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Administrador de Citas {''}
+      <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+     
     </SafeAreaView>
 )};
 
-
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#f3f4f6',
+    flex:1
+  },  
+  titulo:{
+    textAlign:'center',
+    fontSize:30,
+    color:'#374151',
+    fontWeight:'600'
+  },
+  tituloBold:{
+    fontWeight:'900',
+    color:'#6d28d9'
+  }
+})
 
 export default App;
